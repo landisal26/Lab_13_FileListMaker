5cfac3dec3b0f272399e6990c5195310a61c0179
landisal <landisal@mail.uc.edu>
2022-12-05 19:12:46 -0500

    Fin

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,72 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import javax.swing.JFileChooser;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+
+        ArrayList<String> lines = new ArrayList<>();
+
+        try
+        {
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+
+                int line = 0;
+                int words = 0;
+                long characters = selectedFile.length();
+
+                while(reader.ready())
+                {
+                    rec = reader.readLine();
+                    String str[] = rec.split(" ");
+                    words = words + str.length;
+                    line++;
+
+                }
+                System.out.println(selectedFile.getName());
+                System.out.println("Number of lines: " + line);
+                System.out.println("Number of words: " + words);
+                System.out.println("Number of characters: " + characters);
+
+
+                reader.close();
+            }
+            else
+            {
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
+                System.exit(0);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
+
 }
